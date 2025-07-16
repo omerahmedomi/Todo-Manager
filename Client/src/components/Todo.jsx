a61f03077a33df3882a97f127357e91fd86765a9
Umer <omiomeromi11@gmail.com>
2025-07-16 22:01:11 +0300

    Configure for dark mode

--- Client/src/components/Todo.jsx
+++ Client/src/components/Todo.jsx
@@ -4,14 +4,16 @@ const Todo = () => {
   return (
     <div
       className="todo-list bg-[#F8FAFC] flex flex-col
-    justify-between sm:flex-row p-4 rounded-lg border border-light-cyan space-y-4 sm:space-y-0"
+    justify-between sm:flex-row p-4 rounded-lg border border-light-cyan space-y-4 sm:space-y-0  dark:border-light-dark-blue
+    dark:bg-dark-blue
+    "
     >
       <p className="todo font-eczar">Lorem sdfhdf fdklfjd kdfjkd f </p>
       <div className="btns flex gap-4 font-grenze font-bold *:cursor-pointer ">
-        <button className="bg-blue-600 text-white rounded-md px-4 py-0.5">
+        <button className="bg-blue-600 text-white rounded-md px-4 py-0.5 dark:bg-blue-400 dark:text-black">
           Done
         </button>
-        <button className="bg-stone-200 text-blue-600 rounded-md px-4 py-0.5">
+        <button className="bg-stone-200 text-blue-600 rounded-md px-4 py-0.5 dark:bg-blue-900 dark:text-blue-300">
           Delete
         </button>
       </div>
--- Client/src/components/Todos.jsx
+++ Client/src/components/Todos.jsx
@@ -13,27 +13,31 @@ const customBorder = { borderBottom: "solid 1px blue " };
     
     }
   return (
-    <div className="p-4 flex flex-col space-y-4 flex-nowrap max-w-[800px] mx-auto">
-      <h1 className="text-3xl font-grenze font-bold bg-gradient-to-r from-violet-600 to-violet-300 text-transparent bg-clip-text">
-        You have 0 open task.
+    <div className="p-4 flex flex-col space-y-4 flex-nowrap max-w-[800px] mx-auto dark:text-white">
+      <h1 className="text-3xl font-grenze font-bold bg-gradient-to-r from-violet-600 to-violet-300 text-transparent bg-clip-text ">
+        You have 2 open tasks.
       </h1>
-      <ul className="navs flex gap  *:pb-4   border-r-3 border-b-[1px] border-stone-400 *:cursor-pointer  *:px-2 sm:*:px-4 gap-2 sm:gap-4 *:font-grenze *:font-bold *:text-lg flex-wrap transition-all duration-300">
+      <ul
+        className="navs flex gap  *:pb-4   border-r-3 border-b-[1px] border-stone-400 
+      *:cursor-pointer  *:px-2 dark:border-stone-800 sm:*:px-4 gap-2 sm:gap-4 *:font-grenze *:font-bold *:text-lg flex-wrap transition-all duration-300"
+      >
         {tabs.map((tab, index) => (
           <li
             style={activeTab == index ? customBorder : {}}
             onClick={() => {
-              setActiveTab(index)
+              setActiveTab(index);
             }}
           >
-           {tab}
+            {tab}
             <span className="font-eczar text-[#727272] font-[300]"> ( 1 )</span>
           </li>
         ))}
-
-       
       </ul>
-      <div className="todos hidden"></div>
-      <Todo />
+      <div className="todos space-y-4">
+        <Todo />
+        <Todo />
+      </div>
+
       <div className="add-todo flex gap-4 ">
         <Input placeholder="Add task" />
         <span>
