859b2e03d3e097a1b6f98cb36ecf7788a462332c
Umer <omiomeromi11@gmail.com>
2025-07-16 21:26:51 +0300

    Make todo tabs have border bottom when selected

--- Client/src/components/Todo.jsx
+++ Client/src/components/Todo.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Todo = () => {
   return (
-    <div className="todo-list bg-red-300 flex flex-col
-    justify-between sm:flex-row p-4 items-center">
-        <p className="todo ">Lorem s </p>
-        <div className="btns flex gap-3">
-            <button>Done</button>
-            <button>Delete</button>
-        </div>
+    <div
+      className="todo-list bg-[#F8FAFC] flex flex-col
+    justify-between sm:flex-row p-4 rounded-lg border border-light-cyan space-y-4 sm:space-y-0"
+    >
+      <p className="todo font-eczar">Lorem sdfhdf fdklfjd kdfjkd f </p>
+      <div className="btns flex gap-4 font-grenze font-bold *:cursor-pointer ">
+        <button className="bg-blue-600 text-white rounded-md px-4 py-0.5">
+          Done
+        </button>
+        <button className="bg-stone-200 text-blue-600 rounded-md px-4 py-0.5">
+          Delete
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
