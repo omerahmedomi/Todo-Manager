2890abfdfbfbef57c3a22c17d71ac20dd1bfd8ad
Umer <omiomeromi11@gmail.com>
2025-07-17 22:20:57 +0300

    Handle user authentication with token and better error handling

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,44 +1,119 @@
-import { useState } from 'react';
-import './App.css'
-import Button from './components/Button';
-import Input from './components/Input';
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  const [isRegistration,setIsRegistration]=useState(false)
-  const [inputs,setInputs]=useState({})
-  const [error,setError]=useState()
+  const [isRegistration, setIsRegistration] = useState(false);
+  const [inputs, setInputs] = useState({});
+  const [error, setError] = useState();
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isCheckingAuth, setIsCheckingAuth] = useState(true);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("/todos");
+    } else {
+      setIsCheckingAuth(false); // Only allow render if no token
+    }
+  }, []);
+
+  if (isCheckingAuth) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center text-lg dark:text-white font-eczar">
+        Loading...
+      </div>
+    );
+  }
+
+  const apiBase = "http://localhost:2000";
+
   // const set
-  function handleChange(e){
-     setError('')
-     setInputs((prev) => ({
-       ...prev,
-       [e.target.name]: e.target.value,
-     }));
-
+  function handleChange(e) {
+    setError("");
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
   }
 
-  function authenticate(){
-    const {email,password}=inputs;
+  async function authenticate() {
+    const { email, password } = inputs;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if(!email || !password){
-      setError('Pleasae provide the necessary credentials!')
-      return
+    if (!email || !password) {
+      setError("Pleasae provide the necessary credentials!");
+      return;
     }
-    
-    if(email && password && password.length <8){
-      setError('Password must be atleast 8 characters!')
-      return
+
+    if (email && emailRegex.test(email) && password && password.length < 8) {
+      setError("Password must be atleast 8 characters!");
+      return;
     }
-    if (email && password && password.length <8 && !emailRegex.test(email)) {
+    if (email && password && !emailRegex.test(email)) {
       setError("Invalid email!");
-      return
+      return;
     }
 
+    try {
+      //  navigate('/todos')
+      setIsAuthenticating(true);
+      setError("");
 
+      let data;
+      if (isRegistration) {
+        const response = await axios.post(apiBase + "/auth/register", {
+          username: email,
+          password: password,
+        });
 
+        data = response.data;
+        console.log("incoming responser", data);
+      } else {
+        const response = await axios.post(apiBase + "/auth/login", {
+          username: email,
+          password: password,
+        });
+        data = response.data;
+        console.log("incoming responser", data);
+      }
+
+      // if (data.token) {
+      //   token = data.token;
+      //   localStorage.setItem("token", token);
+
+      //   // authenicating into loading
+      //   // authBtn.innerText = "Loading...";
+
+      //   // // fetch todos
+      //   // await fetchTodos();
+
+      //   // // show dashboard
+      //   // showDashboard();
+      //   navigate('/todos')
+      // } else {
+      //   throw Error("❌ Failed to authenticate...");
+      // }
+      if (data.token) {
+        localStorage.setItem("token", data.token);
+        navigate("/todos");
+      } else {
+        throw new Error("❌ Failed to authenticate...");
+      }
+
+      navigate("/todos");
+    } catch (error) {
+      console.log(error);
+      setError(error.response?.data?.message || error.message);
+    } finally {
+      setIsAuthenticating(false);
+    }
   }
-  console.log(inputs)
+  console.log(inputs);
   return (
     <div className="dark:bg-black min-h-svh dark:text-white bg-white text-black flex flex-col sm:w-[633px] mx-auto p-4 justify-center gap-5">
       <div className="flex flex-col ">
@@ -46,12 +121,14 @@ function App() {
           {isRegistration ? "Sign Up" : "Login"}
         </h1>
         <p className="font-eczar leading-6">Create an account!</p>
-        <p className="error font-eczar h-2 text-red-500 text-sm">{error||''}</p>
+        <p className="error font-eczar h-2 text-red-500 text-sm">
+          {error || ""}
+        </p>
       </div>
 
       <div className=" flex flex-col items-start gap-5 font-eczar ">
         <Input
-          name="email"        
+          name="email"
           placeholder="Email"
           onChange={handleChange}
           value={inputs.email}
@@ -64,7 +141,12 @@ function App() {
           value={inputs.password}
           type="password"
         />
-        <Button text="Submit" btnFunction={()=>{authenticate()}}/>
+        <Button
+          text={isAuthenticating ? "Authenticating" : "Submit"}
+          btnFunction={() => {
+            authenticate();
+          }}
+        />
       </div>
       <hr className="text-light-cyan dark:text-[#626368]" />
       <div className=" flex items-center font-eczar gap-4">
---
+++ Server/src/server.js
@@ -0,0 +1,80 @@
+import express from "express";
+import cors from "cors";
+import {Pool} from 'pg'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from "dotenv";
+dotenv.config();
+
+
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "Todo",
+  password: "Omer$$@1641",
+  port: 5432,
+});
+
+
+ 
+const app=express()
+ const port=2000
+
+ app.use(cors());
+ app.use(express.json());
+
+
+app.post("/auth/register", async (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+  const hashedPassword=bcrypt.hashSync(password,10)
+  try{
+    
+    const checkDuplication = await pool.query(
+      `SELECT * FROM users WHERE username = $1 `,
+      [username]
+    );
+       if(checkDuplication.rows[0]){ return res.status(409).send({message:"User already exists!, Try logging in."})}
+    const result = await pool.query(`INSERT INTO users(username,password) VALUES ($1,$2) RETURNING *`,[username,hashedPassword])
+    const token= jwt.sign({id:result.rows[0].id},process.env.JWT_SECRET,{expiresIn:'24h'})
+    res.json({token})
+  }catch(error){
+    console.log(error)
+    res.status(503).json({ error: error.message });
+
+  }
+  // res.status(201).json({ message: "User created" }); // ✅ send response
+});
+app.post("/auth/login", async(req, res) => {
+   const { username, password } = req.body;
+  console.log(username, password);
+  try{
+
+    const result = await pool.query(`SELECT * FROM users WHERE username = $1 `,[username])
+    const user=result.rows[0]
+    if(!user){
+      return res.status(404).send({message:"User not found!"})
+    }
+
+    const passwordIsValid=bcrypt.compareSync(password,user.password)
+
+    if(!passwordIsValid){
+      return res.status(404).send({message:"Incorrect Password"})
+    }
+    //now user is authenticated
+    const token= jwt.sign({id:user.id},process.env.JWT_SECRET,{expiresIn:'24h'})
+    res.json({token})
+  }catch(error){
+    console.log(error)
+    res.status(503).json({ error: error.message });
+
+    
+  }
+});
+
+
+app.listen(port,()=>{
+    console.log("Running on port",port)
+})
+
