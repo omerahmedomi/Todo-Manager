0873d8280a39e4abea6f1e41770a20519bde9db7
Umer <omiomeromi11@gmail.com>
2025-07-17 08:20:32 +0300

    Handle input validation from client

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,28 +2,83 @@ import { useState } from 'react';
 import './App.css'
 import Button from './components/Button';
 import Input from './components/Input';
+import axios from 'axios'
+
 function App() {
   const [isRegistration,setIsRegistration]=useState(false)
+  const [inputs,setInputs]=useState({})
+  const [error,setError]=useState()
+  // const set
+  function handleChange(e){
+     setError('')
+     setInputs((prev) => ({
+       ...prev,
+       [e.target.name]: e.target.value,
+     }));
+
+  }
+
+  function authenticate(){
+    const {email,password}=inputs;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!email || !password){
+      setError('Pleasae provide the necessary credentials!')
+      return
+    }
+    
+    if(email && password && password.length <8){
+      setError('Password must be atleast 8 characters!')
+      return
+    }
+    if (email && password && password.length <8 && !emailRegex.test(email)) {
+      setError("Invalid email!");
+      return
+    }
+
+
+
+  }
+  console.log(inputs)
   return (
     <div className="dark:bg-black min-h-svh dark:text-white bg-white text-black flex flex-col sm:w-[633px] mx-auto p-4 justify-center gap-5">
       <div className="flex flex-col ">
         <h1 className="font-grenze font-bold text-2xl leading-8 sm:text-3xl">
-          {isRegistration?"Sign Up":"Login"}
+          {isRegistration ? "Sign Up" : "Login"}
         </h1>
         <p className="font-eczar leading-6">Create an account!</p>
+        <p className="error font-eczar h-2 text-red-500 text-sm">{error||''}</p>
       </div>
 
       <div className=" flex flex-col items-start gap-5 font-eczar ">
-        <Input placeholder="Email" />
-        <Input placeholder="Password" />
-        <Button text="Submit" />
+        <Input
+          name="email"        
+          placeholder="Email"
+          onChange={handleChange}
+          value={inputs.email}
+          type="email"
+        />
+        <Input
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          value={inputs.password}
+          type="password"
+        />
+        <Button text="Submit" btnFunction={()=>{authenticate()}}/>
       </div>
       <hr className="text-light-cyan dark:text-[#626368]" />
       <div className=" flex items-center font-eczar gap-4">
-        <p className="text-sm sm:text-base">{isRegistration?"Already have an account?":"Don't have an account?"}</p>
-        <Button text={isRegistration?"Login":"Sign Up"} btnFunction={()=>{
-           setIsRegistration((prev)=>!prev)
-        }} />
+        <p className="text-sm sm:text-base">
+          {isRegistration
+            ? "Already have an account?"
+            : "Don't have an account?"}
+        </p>
+        <Button
+          text={isRegistration ? "Login" : "Sign Up"}
+          btnFunction={() => {
+            setIsRegistration((prev) => !prev);
+          }}
+        />
       </div>
     </div>
   );
--- Client/src/components/Input.jsx
+++ Client/src/components/Input.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Input = ({placeholder}) => {
+const Input = ({placeholder,onChange,value,type,name}) => {
+  // const [inputVal,setInputVal]=useState('')
+  // function handleChange(e){
+  //   setInputVal(e.target.value)
+  //   console.log(e.target.name,e.target.value)
+
+    
+
+  // }
+   
   return (
+   
     <label htmlFor="" className="w-full ">
       <input
-        type="text"
+        type={type}
         className="border-1 rounded-lg p-3 bg-[#F8FAFC] focus:outline-none text-sm w-full border-light-cyan 
         dark:border-light-dark-blue
         hover:border-dark-cyan focus:border-dark-cyan
@@ -12,7 +22,11 @@ const Input = ({placeholder}) => {
         dark:focus:border-[#4649AF]
         dark:bg-dark-blue
         "
+        required
+        name={name}
+        value={value}
         placeholder={placeholder}
+        onChange={(e)=>{onChange(e)}}
       />
     </label>
   );
